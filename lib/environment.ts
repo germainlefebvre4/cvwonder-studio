c4f95288b312d9b62a444c16f447e38f05777844
Germain LEFEBVRE <germain.lefebvre4@gmail.com>
2025-04-11 10:08:33 +0200

    chore: rename APP_ENV envvar

--- lib/environment.ts
+++ lib/environment.ts
@@ -2,7 +2,7 @@ import { LogLevel } from "./logger";
 
 export type Environment = 'development' | 'production';
 
-export const APP_ENV: Environment = process.env.REACT_APP_APP_ENV === 'production' ? 'production' : 'development';
+export const APP_ENV: Environment = process.env.APP_ENV === 'production' ? 'production' : 'development';
 export const LOG_LEVEL: LogLevel = process.env.LOG_LEVEL as LogLevel || 'info';
 export const CVWONDER_VERSION: string = process.env.CVWONDER_VERSION || '0.3.0';
 export const CVWONDER_PDF_GENERATION_PORT: number = parseInt(process.env.CVWONDER_PDF_GENERATION_PORT || '9889', 10);
